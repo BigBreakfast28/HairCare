2b472d17765bcd7e1509c98baebff0a402a97ad5
Cameron <deancameron99@gmail.com>
2023-08-17 23:22:10 -0400

    Starting a new ecommerce site for a client

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { DESTRUCTION } from "dns";
+
+export default function Home() {
+    return (
+     <div className="bg-pink-800 w-screen h-screen flex items-center ">
+        <div className="text-center w-full">
+            <Button className="">
+                Click me
+            </Button>
+        </div>
+     </div>
+    )
+  }
+  
