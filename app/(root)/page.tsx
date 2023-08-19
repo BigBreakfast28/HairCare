ab4fb19896930dcebac7b5a607ace52cb333da67
Cameron <deancameron99@gmail.com>
2023-08-18 22:36:33 -0400

    Finishing authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { DESTRUCTION } from "dns";
 
 export default function Home() {
     return (
-     <div className="bg-pink-800 w-screen h-screen flex items-center ">
-        <div className="text-center w-full">
-            <Button className="">
-                Sign In
-            </Button>
-        </div>
-     </div>
+    <div>
+        <Button>Sign in</Button>
+    </div>
     )
   }
   
