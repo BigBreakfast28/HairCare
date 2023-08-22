03c488bfb5a2f24dc8fddc77f89a3e55a8e09162
Cameron <deancameron99@gmail.com>
2023-08-22 08:31:57 -0400

    adding 'submit' button to from

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,8 +6,10 @@ import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal"
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 
 const formSchema = z.object({
     name: z.string().min(1),
@@ -24,7 +26,8 @@ export const StoreModal = () => {
     });
 
     const onSubmit = async (values:z .infer<typeof formSchema>) => {
-    console.log(values);}
+    console.log(values);
+}
 
     return (
         <Modal 
@@ -46,9 +49,16 @@ export const StoreModal = () => {
                                     <FormControl>
                                         <Input placeholder="E-Commerce"{...field}/>
                                     </FormControl>
+                                    <FormMessage>
+
+                                    </FormMessage>
                                 </FormItem>
                             )}
                             />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button variant="outline" onClick={storeModal.onClose}>Cancel</Button>
+                                <Button type="submit">Continue</Button>
+                            </div>
                         </form>
                     </Form>
                 </div>
