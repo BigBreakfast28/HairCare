048459e98611616b06703d902fb8008d3e31cda3
Cameron <deancameron99@gmail.com>
2023-08-25 00:02:15 -0400

    Adding popover/command functions

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,8 +1,16 @@
-import { PopoverTrigger } from "@/components/ui/popover";
-import { useStoreModal } from "@/hooks/use-store-modal";
+"use client";
 
+import { Button } from "./ui/button";
+import { PopoverTrigger, Popover, PopoverContent } from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { Store } from "@prisma/client";
+
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Command, CommandList, CommandInput } from "@/components/ui/command";
+
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
 
@@ -23,9 +31,39 @@ const formattedItems = items.map((item) => ({
     value: item.id
 }));
 
+    const currentStore = formattedItems.find((item) => item.value === params.storeId);
+
+    const [open, setOpen] = useState(false);
+    
+    const onStoreSelect = (store: {value: string, label:string}) => {
+       setOpen(false);
+       router.push(`/${store.value}`);
+
+    }
+
     return (
-        <div>
-        Store Switcher
-        </div>
+       <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>    
+                <Button 
+                variant="outline"
+                size="sm"
+                role="combobox"
+                aria-expanded={open}
+                aria-label="Select a store"
+                className={cn("w-[200px] justify-between", className)}
+                >
+                    <StoreIcon  className="mr-2 h-4 w-4 "/>
+                    Current Store 
+                    <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+                <Command>
+                    <CommandList>
+                        <CommandInput placeholder ="Search store..."/>
+                    </CommandList>
+                </Command>
+            </PopoverContent>
+       </Popover>
     );
 };
