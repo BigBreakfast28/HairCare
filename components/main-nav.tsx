b2fe43f8ccdfba0f07d9f4a05df765c8da955ab0
Cameron <deancameron99@gmail.com>
2023-11-19 21:19:40 -0500

    Adding 'sizes' to dashboard

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -29,7 +29,12 @@ export function MainNav ({
             href:`/${params.storeId}/categories`,
             label:"Catergories",
             active: pathname === `/${params.storeId}/categories`
-            },
+        },
+        {
+            href:`/${params.storeId}/sizes`,
+            label:"Sizes",
+            active: pathname === `/${params.storeId}/sizes`
+        },
         {
         href:`/${params.storeId}/settings`,
         label:'Settings',
