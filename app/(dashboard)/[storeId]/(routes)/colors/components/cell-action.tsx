c4006104be43487edb2df473caf0b5de382923d2
Cameron <deancameron99@gmail.com>
2023-11-20 23:22:58 -0500

    Finished Color tab

--- app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -16,11 +16,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { AlertModal } from "@/components/modals/alert-modal";
 
-import { SizeColumn } from "./columns";
+import { ColorColumn } from "./columns";
 
 
 interface CellActionProps {
-  data: SizeColumn;
+  data: ColorColumn;
 }
 
 export const CellAction: React.FC<CellActionProps> = ({
@@ -35,11 +35,11 @@ export const CellAction: React.FC<CellActionProps> = ({
   const onConfirm = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
-      toast.success('Size deleted.');
+      await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
+      toast.success('Color deleted.');
       router.refresh();
     } catch (error) {
-      toast.error('Make sure you removed all products using this size first.');
+      toast.error('Make sure you removed all products using this color first.');
     } finally {
       setOpen(false);
       setLoading(false);
@@ -48,7 +48,7 @@ export const CellAction: React.FC<CellActionProps> = ({
 
   const onCopy = (id: string) => {
     navigator.clipboard.writeText(id);
-    toast.success('Size ID copied to clipboard.');
+    toast.success('Color ID copied to clipboard.');
   }
 
   return (
@@ -74,7 +74,7 @@ export const CellAction: React.FC<CellActionProps> = ({
             <Copy className="mr-2 h-4 w-4" /> Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}>
+            onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}>
             <Edit className="mr-2 h-4 w-4" /> 
             Update
           </DropdownMenuItem>
---
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,38 @@
+import prismadb from "@/lib/prismadb";
+
+import { ColorsClient} from "./components/client";
+import { ColorColumn } from "./components/columns";
+
+import {format} from "date-fns"
+
+const ColorPage = async ({
+    params 
+}: {
+    params: { storeId: string }
+}) => {
+    const colors = await prismadb.color.findMany({
+        where: {
+            storeId: params.storeId
+        },
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+
+    const formattedColors: ColorColumn[] = colors.map((item) => ({
+        id: item.id,
+        name: item.name,
+        value: item.value,
+        createdAt: format(item.createdAt, "MMMM do, yyyy")
+    }))
+
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y p-8 pt-6">
+                <ColorsClient data={formattedColors}/>
+            </div>
+        </div>
+    );
+}
+
+export default ColorPage;
